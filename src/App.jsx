308a1881a3cc1fa03fefdae67822c310bccea23d
Karthik Rishinarada <karthikrk11135@gmail.com>
2024-10-01 13:19:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h3 className="mx-10 p-2 font-medium text-center border rounded-md shadow-md mb-4">
+        Mess
+      </h3>
+      <div className="mx-10  py-5 border rounded-t-lg p-2">
+        <div className="text-center font-medium text-3xl mx-4 px-4 pb-1.5 rounded-2xl shadow-xl">
+          <div className="">
+            <img className="scale-75 rounded" src="/profilepic.jpeg"></img>
+          </div>
+          <h1>
+            Hi, RK <br></br> KARTHIKEYA SHASHANK (110121075).
+          </h1>
+          <p>
+            You have <br></br> been <br></br> allocated <br></br> Sabari Mess{" "}
+            <br></br> for October <br></br> 2024.
+          </p>
+          <hr className="mt-12 mb-5 font-extralight"></hr>
+        </div>
+      </div>
+      <div className="text-center font-extralight py-3  w-full bottom-0 border">
+        Made with <span className="text-sm">🖤</span> by{" "}
+        <span className="text-blue-500">Delta Force</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='mx- mt-3 mb-2'>
+        <img src="/navbar.jpeg" className='w-full -translate-x-2'></img>
+    </div>
+  )
+}
+
+export default Navbar
